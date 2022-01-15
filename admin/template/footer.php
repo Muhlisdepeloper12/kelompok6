67caeaab9d90b56669f50994ae4fc22da9759ea3
usran12 <laodeusran@gmail.com>
2022-01-14 19:29:39 -0800

    footer.php

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -3,7 +3,6 @@
       <footer class="site-footer">
           <div class="text-center">
               <?php echo date('Y');?> - Sistem Penjualan Barang Berbasis Web | 
-              By <a href="https://codekop.com" style="color:yellow;font-weight:700;" target="_blank">Codekop.com</a>
               <a href="#" class="go-top">
                   <i class="fa fa-angle-up"></i>
               </a>
